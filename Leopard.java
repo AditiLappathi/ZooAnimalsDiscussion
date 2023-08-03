99753e98f90578c01ac9e1756e8a8c21e2b9c289
AditiLappathi <141365437+AditiLappathi@users.noreply.github.com>
2023-08-03 12:53:41 -0700

    Create Leopard.java

---
+++ Leopard.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zooanimals;
+
+/**
+ *
+ * @author lmeade, aditi
+ */
+public interface iAnimal {
+    public String getAnimalType();
+    public int getIdTag();
+    public void setIdTag(int anIdTag);
+    public int getMinTemperature();
+    public int getMaxTemperature();
+}
+
+public class Leopard {
+
+}
